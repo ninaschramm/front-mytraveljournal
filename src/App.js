6b026a8592ea47c1222f54b34b1a4c92ba02a68a
ninaschramm <schrammnina@outlook.com>
2023-01-26 13:17:07 -0300

    server initial config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import styled from 'styled-components';
+
+import useToken from './hooks/useToken';
+
+import bgimage from './assets/images/bg-image.png';
+import { UserProvider } from './contexts/UserContext';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (  
+    <Container >
+      <UserProvider>
+        <Router>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRouteGuard>
+                    <Dashboard />
+                  </ProtectedRouteGuard>
+                }
+              ></Route>
+          </Routes>
+        </Router>
+      </UserProvider>      
+    </Container>      
   );
 }
 
+function ProtectedRouteGuard({ children }) {
+  const token = useToken();
+
+  if (!token) {
+    return <Navigate to="/sign-in" />;
+  }
+
+  return <>
+    {children}
+  </>;
+}
+
 export default App;
+
+const Container = styled.div`
+  padding: 30px;
+  height: 100vh;
+  width: 100%;
+  background: url(${bgimage});
+  background-size: cover; 
+  display: flex;
+  justify-content: center;
+`;
